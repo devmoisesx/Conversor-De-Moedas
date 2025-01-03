fa0afb3bec52a818c633e98ce352e70e08afb4aa
moisesdev <secondmox91@gmail.com>
2025-01-03 15:07:59 -0300

    Remove variavel nao utilizada

--- src/com/conversormoedas/api/RequestApi.java
+++ src/com/conversormoedas/api/RequestApi.java
@@ -9,7 +9,6 @@ import com.google.gson.GsonBuilder;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -24,35 +23,30 @@ public class RequestApi {
         var json = response("pair", dados);
         Gson gson = new GsonBuilder().create();
         ExchangeApi respostaApi = gson.fromJson(json, ExchangeApi.class);
-        Exchange convecaoDeMoeda = new Exchange(respostaApi);
-        return convecaoDeMoeda;
+        return new Exchange(respostaApi);
     }
 
     public static ConversionRates taxaCambio(String moedaBase) {
         var json = response("latest", moedaBase);
         Gson gson = new GsonBuilder().create();
         ConversionRatesApi conversionRatesApi = gson.fromJson(json, ConversionRatesApi.class);
-        ConversionRates taxasCambio = new ConversionRates(conversionRatesApi);
-        return taxasCambio;
+        return new ConversionRates(conversionRatesApi);
     }
 
     private static String response(String metodo, String dados) {
         try {
             String urlString = "https://v6.exchangerate-api.com/v6/" + apiKey + "/" + metodo + "/" + dados;
-            URL url = new URL(urlString);
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(urlString))
                     .GET()
                     .build();
-
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             return response.body();
         } catch (Exception e) {
             System.out.println("Error em:");
             System.out.println(e.getMessage());
-            System.out.println(e.getCause());
         }
         return null;
     }
